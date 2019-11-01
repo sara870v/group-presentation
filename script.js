75485498fb461f3c508364f008358a26ba33275d
Natalie Sirich <nata2653@stud.kea.dk>
2019-11-01 21:42:57 +0100

    add natalie img

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ document.querySelector("button").addEventListener("click", removeStart);
 
 function removeStart() {
   document.querySelector("#start-container").classList.add("hide");
+  document.querySelector(".intro").classList.remove("hidden");
 }
+
+const animated = document.querySelector(".spotlight-enter-active");
+
+animated.addEventListener("animationend", () => {
+  console.log("Animation ended");
+  document.querySelector(".natalie-name").classList.remove("hidden");
+  document.querySelector(".natalie-name").classList.add("animation-start");
+  animated.classList.add("natalie-name");
+});
