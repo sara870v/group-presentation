f09702fe2f5a393f364db657f4741568df884803
sara870v <44156928+sara870v@users.noreply.github.com>
2019-11-03 11:17:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.querySelector("button").addEventListener("click", removeStart);
 function removeStart() {
   document.querySelector("#start-container").classList.add("hide");
   document.querySelector(".intro").classList.remove("hidden");
+  document.querySelector("#musik").play();
 }
 
 const animated = document.querySelector(".spotlight-enter-active");
@@ -14,4 +15,18 @@ animated.addEventListener("animationend", () => {
   document.querySelector(".natalie-name").classList.remove("hidden");
   document.querySelector(".natalie-name").classList.add("animation-start");
   animated.classList.add("natalie-name");
+
+  setTimeout(myFunction, 3000);
 });
+
+function myFunction() {
+  document.querySelector(".intro").classList.add("hidden");
+  document.querySelector(".info").classList.remove("hidden");
+  setTimeout(myFunction2, 3000);
+}
+
+function myFunction2() {
+  document.querySelector(".info").classList.add("hidden");
+  document.querySelector(".sarah").classList.remove("hidden");
+  document.querySelector(".sarah-name").classList.remove("hidden");
+}
